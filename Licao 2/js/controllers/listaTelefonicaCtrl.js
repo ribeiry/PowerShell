bb542c57528d5439a16af227f5db991890327945
Jose Augusto <ribeiry10@hotmail.com>
2017-08-02 16:08:23 -0300

    Alterações nos projetos construindo serviços do tipo: - Provider-Factory-Generator

--- Licao 2/js/controllers/listaTelefonicaCtrl.js
+++ Licao 2/js/controllers/listaTelefonicaCtrl.js
@@ -1,10 +1,10 @@
-angular.module("listaTelefonica").controller("listaTelefonicaCtrl",function($scope, $filter,$http){
+angular.module("listaTelefonica").controller("listaTelefonicaCtrl",function($scope, $filter,contatosAPI,operadoraAPI,serialGenerator){
        $scope.app="Lista Telefonica";
        $scope.contatos = [];
        $scope.operadoras=[];
                 
         var carregarContatos = function(){
-            $http.get("http://localhost:3412/contatos").then(function (data){
+           contatosAPI.getContatos().then(function (data){
             $scope.contatos = data.data;
              },function(data,status){
                 $scope.message = "Aconteceu um problema: " + data.status+ " - " +  data.statusText  ;
@@ -12,13 +12,14 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl",function($sco
         };
 
         var carregarOperadoras = function(){
-            $http.get("http://localhost:3412/operadoras").then(function(data){
+            operadoraAPI.getOperadoras().then(function(data){
             $scope.operadoras = data.data;
             });
         };
         $scope.adicionarContato= function(contato) {
+            contato.serial = serialGenerator.generate();
             contato.data = new Date();
-            $http.post("http://localhost:3412/contatos",contato).then(function (data){
+            contatosAPI.saveContato(contato).then(function (data){
                 delete $scope.contato;
                 $scope.contatoForm.$setPristine();
                 carregarContatos();

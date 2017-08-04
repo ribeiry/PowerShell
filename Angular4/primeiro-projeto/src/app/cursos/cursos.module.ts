7f925e136c4bb5d9128de7b90f01d5499adfa0eb
Jose Augusto <ribeiry10@hotmail.com>
2017-08-03 22:51:04 -0300

    feito o curso de angular

---
+++ Angular4/primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CursosComponent } from './cursos.component';
+import { CursosDetalheComponent } from "./cursos-detalhe/cursos-detalhe.component";
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    CursosComponent,
+    CursosDetalheComponent
+  ],
+  exports:[
+    CursosComponent,
+    CursosDetalheComponent 
+  ]
+})
+export class CursosModule { }
